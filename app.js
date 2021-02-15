de5f0651fb3bab89c06b4364b711bd558e32ad03
D0641604 <xu3g4pln@gmail.com>
2021-02-15 15:25:01 +0800

    Add logout authentication

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // Flash
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,11 @@ router.post(
             const { email, username, password } = req.body;
             const user = new User({ email, username });
             const registeredUser = await User.register(user, password);
-            req.flash("success", "Welcome to Yelp Camp!");
-            res.redirect("/campgrounds");
+            req.login(registeredUser, (err) => {
+                if (err) return next(err);
+                req.flash("success", "Welcome to Yelp Camp!");
+                res.redirect("/campgrounds");
+            });
         } catch (e) {
             req.flash("error", e.message);
             res.redirect("/register");
@@ -29,7 +32,15 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    delete req.session.returnTo;
     req.flash("success", "welcome back!");
+    res.redirect(redirectUrl);
+});
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.flash("success", "Goodbye!");
     res.redirect("/campgrounds");
 });
 
